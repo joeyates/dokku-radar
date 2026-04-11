8b5a08c450b7a5068f09d775877148943c0490ff
Joe Yates <joe.g.yates@gmail.com>
2026-04-11 10:01:46 +0200

    Fix service listing

--- lib/dokku_radar/dokku_cli.ex
+++ lib/dokku_radar/dokku_cli.ex
@@ -104,24 +104,5 @@ defmodule DokkuRadar.DokkuCli do
     output
     |> String.split("\n", trim: true)
     |> Enum.reject(&String.starts_with?(&1, "====="))
-    |> Enum.reject(&(&1 |> String.trim() |> String.starts_with?("NAME")))
-    |> Enum.map(&parse_service_line/1)
-    |> Enum.reject(&is_nil/1)
-  end
-
-  defp parse_service_line(line) do
-    case String.split(line) do
-      [name, _version, status | rest] ->
-        links =
-          rest
-          |> Enum.reject(&(&1 == ""))
-          |> Enum.flat_map(&String.split(&1, ","))
-          |> Enum.reject(&(&1 == ""))
-
-        %{name: name, status: status, links: links}
-
-      _ ->
-        nil
-    end
   end
 end
