66f2d4656eb45e4a58a3f1780e7ffef5377bc020
Joe Yates <joe.g.yates@gmail.com>
2026-04-11 17:24:53 +0200

    Configure environment

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,4 +1,24 @@
 import Config
 
-config :dokku_radar, port: "PORT" |> System.get_env("9110") |> String.to_integer()
-config :dokku_radar, dokku_host: System.get_env("DOKKU_HOST", "localhost")
+app_name = :dokku_radar
+
+ssh_certificate_path =
+  System.get_env("SSH_CERTIFICATE_PATH") ||
+    raise """
+    environment variable SSH_CERTIFICATE_PATH is missing.
+    For example: /data/.ssh/id_ed25519
+    """
+
+dokku_host =
+  System.get_env("DOKKU_HOST") ||
+    raise """
+    environment variable DOKKU_HOST is missing
+    """
+
+port = "PORT" |> System.get_env("9110") |> String.to_integer()
+
+config app_name, DokkuRadar.DokkuCli,
+  dokku_host: dokku_host,
+  ssh_certificate_path: ssh_certificate_path
+
+config app_name, dokku_host: config(app_name, port: port)
