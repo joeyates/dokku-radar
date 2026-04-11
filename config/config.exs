9824c7141e90fa48ddbad6115991c3e6d03efc1a
Joe Yates <joe.g.yates@gmail.com>
2026-04-11 17:25:02 +0200

    Fix logging

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@ import Config
 
 config :logger, :default_handler, level: :debug
 
-config :logger, :default_formatter, format: "$time $message $metadata"
+config :logger, :default_formatter, format: "$time $message $metadata\n"
 
 import_config "#{config_env()}.exs"
