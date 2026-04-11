9fccde493f0d02c4cb8e0d0999a9083d3b6cb987
Joe Yates <joe.g.yates@gmail.com>
2026-04-11 17:25:38 +0200

    Only do Dokku calls in CLI module

--- lib/dokku_radar/dokku_cli.ex
+++ lib/dokku_radar/dokku_cli.ex
@@ -1,82 +1,29 @@
 defmodule DokkuRadar.DokkuCli do
-  @behaviour DokkuRadar.DokkuCli.Behaviour
+  # @behaviour DokkuRadar.DokkuCli.Behaviour
 
   require Logger
 
-  @known_service_types ~w(
-    elasticsearch
-    mariadb
-    memcached
-    mongo
-    mongodb
-    mysql
-    postgres
-    rabbitmq
-    redis
-  )
+  @system Application.compile_env(:dokku_radar, :system, System)
 
-  @default_host "localhost"
-  @certificate_path "/data/.ssh/id_ed25519"
+  def call(command, args \\ []) do
+    Logger.debug("Calling Dokku command #{inspect(command)}, with args #{inspect(args)}")
 
-  @impl true
-  def list_service_types(opts \\ []) do
-    cmd_fn = Keyword.get(opts, :cmd_fn, &System.cmd/3)
-    host = Keyword.get(opts, :host, Application.get_env(:dokku_radar, :dokku_host, @default_host))
-
-    Logger.debug("Fetching Dokku plugin list via SSH", host: host)
-
-    case cmd_fn.("ssh", ssh_args(host, "plugin:list"), stderr_to_stdout: true) do
+    case @system.cmd("ssh", ssh_args(command) ++ args, stderr_to_stdout: true) do
       {output, 0} ->
-        types = parse_service_types(output)
-        Logger.info("Fetched Dokku service types", host: host, count: length(types))
-        {:ok, types}
+        {:ok, output}
 
       {output, exit_code} ->
-        Logger.warning("SSH call to list plugins failed",
-          host: host,
-          exit_code: exit_code,
-          output: String.slice(output, 0, 200)
-        )
-
-        {:error, {exit_code, output}}
+        {:error, output, exit_code}
     end
   end
 
-  @impl true
-  def list_services(service_type, opts \\ []) do
-    cmd_fn = Keyword.get(opts, :cmd_fn, &System.cmd/3)
-    host = Keyword.get(opts, :host, Application.get_env(:dokku_radar, :dokku_host, @default_host))
+  defp ssh_args(command) do
+    host = dokku_host()
+    ssh_certificate_path = ssh_certificate_path()
 
-    Logger.debug("Fetching Dokku service list via SSH", host: host, service_type: service_type)
-
-    case cmd_fn.("ssh", ssh_args(host, "#{service_type}:list"), stderr_to_stdout: true) do
-      {output, 0} ->
-        services = parse_services(output)
-
-        Logger.info("Fetched Dokku services",
-          host: host,
-          service_type: service_type,
-          count: length(services)
-        )
-
-        {:ok, services}
-
-      {output, exit_code} ->
-        Logger.warning("SSH call to list services failed",
-          host: host,
-          service_type: service_type,
-          exit_code: exit_code,
-          output: String.slice(output, 0, 200)
-        )
-
-        {:error, {exit_code, output}}
-    end
-  end
-
-  defp ssh_args(host, command) do
     [
       "-i",
-      @certificate_path,
+      ssh_certificate_path,
       "-o",
       "BatchMode=yes",
       "-o",
@@ -90,19 +37,15 @@ defmodule DokkuRadar.DokkuCli do
     ]
   end
 
-  defp parse_service_types(output) do
-    output
-    |> String.split("\n", trim: true)
-    |> Enum.reject(&String.starts_with?(&1, "====="))
-    |> Enum.map(fn line ->
-      line |> String.trim() |> String.split(~r/\s+/) |> List.first()
-    end)
-    |> Enum.filter(&(&1 in @known_service_types))
+  defp module_env!() do
+    Application.fetch_env!(:dokku_radar, __MODULE__)
   end
 
-  defp parse_services(output) do
-    output
-    |> String.split("\n", trim: true)
-    |> Enum.reject(&String.starts_with?(&1, "====="))
+  defp dokku_host() do
+    Keyword.fetch!(module_env!(), :dokku_host)
+  end
+
+  defp ssh_certificate_path() do
+    Keyword.fetch!(module_env!(), :ssh_certificate_path)
   end
 end
