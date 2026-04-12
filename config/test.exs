3f9e50b2bcbadad54de7de9b1bc23d476b7f2a98
Joe Yates <joe.g.yates@gmail.com>
2026-04-12 19:03:34 +0200

    In collector.ex, add compile_env module attributes, simplify collect/1 to collect/0

--- config/test.exs
+++ config/test.exs
@@ -6,6 +6,10 @@ config app_name, port: 9876
 
 config app_name,
   System: System.Mock,
+  "DokkuRadar.DockerClient": DokkuRadar.DockerClient.Mock,
+  "DokkuRadar.FilesystemReader": DokkuRadar.FilesystemReader.Mock,
+  "DokkuRadar.ServiceCache": DokkuRadar.ServiceCache.Mock,
+  "DokkuRadar.Collector": DokkuRadar.Collector.Mock,
   "DokkuRadar.DokkuCli": DokkuRadar.DokkuCli.Mock,
   "DokkuRadar.Service": DokkuRadar.Service.Mock,
   "DokkuRadar.ServicePlugin": DokkuRadar.ServicePlugin.Mock,
