24b0f58fb25ab590ebf1329d7c1d52f8d9cb6f60
Joe Yates <joe.g.yates@gmail.com>
2026-04-12 09:17:47 +0200

    Delete DokkuRadar.DokkuCli.Behaviour

--- lib/dokku_radar/dokku_cli/behaviour.ex
+++
@@ -1,4 +0,0 @@
-defmodule DokkuRadar.DokkuCli.Behaviour do
-  @callback list_service_types(keyword()) :: {:ok, [String.t()]} | {:error, term()}
-  @callback list_services(String.t(), keyword()) :: {:ok, [map()]} | {:error, term()}
-end
