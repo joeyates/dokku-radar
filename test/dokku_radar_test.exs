123332c151124eba79b4aaae63e7a75d3a523714
Joe Yates <joe.g.yates@gmail.com>
2026-04-12 18:23:01 +0200

    Don't test the supervisor

--- test/dokku_radar_test.exs
+++ test/dokku_radar_test.exs
@@ -7,13 +7,4 @@ defmodule DokkuRadarTest do
              fn {app, _, _} -> app == :dokku_radar end
            )
   end
-
-  test "supervisor has Bandit child serving the Router" do
-    children = Supervisor.which_children(DokkuRadar.Supervisor)
-
-    assert Enum.any?(children, fn
-             {{Bandit, _ref}, _pid, _type, [Bandit]} -> true
-             _ -> false
-           end)
-  end
 end
