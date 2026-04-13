5488e5242a3710cc94bfff7c49718a7e22cf65f5
Joe Yates <joe.g.yates@gmail.com>
2026-04-13 11:33:54 +0200

    Update DokkuRadar.Application: replace DokkuRadar.ServiceCache child with DokkuRadar.Services.Cache

--- lib/dokku_radar/application.ex
+++ lib/dokku_radar/application.ex
@@ -6,7 +6,7 @@ defmodule DokkuRadar.Application do
     port = Application.get_env(:dokku_radar, :port, 9110)
 
     children = [
-      DokkuRadar.ServiceCache,
+      DokkuRadar.Services.Cache,
       {Task.Supervisor, name: DokkuRadar.TaskSupervisor},
       {Bandit, plug: DokkuRadar.Router, port: port}
     ]
